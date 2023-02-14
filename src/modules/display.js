31965dacf2f2be205f3a08e71aef6a0d6380c4d6
Ayo Moses <ayomoses@afrimash.com>
2023-02-14 20:25:52 +0100

    fixed linters and made helper functions DRY

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,34 +1,18 @@
 import events from './selectors.js';
 import apiHandler from './fetch.js';
-import BASE_URL from './utils.js';
-
-
-
+import { BASE_URL, createMovieElements } from './utils.js';
 
 const displayMovies = (movies) => {
   movies.forEach((movie) => {
-  const { image } = movie.show;
-  const list = document.createElement('li');
-  const movieName = document.createElement('h3');
-  movieName.textContent = movie.show.name
-  const img = document.createElement('img');
-  const btnContainer = document.createElement('div')
-  const commentBtn = document.createElement('button')
-  const reserveBtn = document.createElement('button')
-  commentBtn.setAttribute('class', 'comment-btn')
-  reserveBtn.setAttribute('class', 'reserve-btn')
-  btnContainer.setAttribute('class', 'movie-actions')
-  commentBtn.textContent = "Comments"
-  reserveBtn.textContent = "Reservations"
-  img.setAttribute('src', image.medium);
-  list.setAttribute('class', 'movie-list');
-  btnContainer.appendChild(reserveBtn)
-  btnContainer.appendChild(commentBtn)
-  list.appendChild(img);
-  list.appendChild(movieName);
-  list.appendChild(btnContainer)
-  events.appendChild(list);
-});
+    const { image, name } = movie.show;
+    const list = document.createElement('li');
+    list.setAttribute('class', 'movie-list');
+    const arrayElements = createMovieElements(image, name);
+    arrayElements.forEach((elem) => {
+      list.appendChild(elem);
+    });
+    events.appendChild(list);
+  });
 };
 
 const getAllEvents = async () => {
--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,3 +1,21 @@
-const BASE_URL = 'https://api.tvmaze.com/search/shows?q=girls';
+export const BASE_URL = 'https://api.tvmaze.com/search/shows?q=girls';
 
-export default BASE_URL;
+export const createMovieElements = (image, name) => {
+  const btnContainer = document.createElement('div');
+  const commentBtn = document.createElement('button');
+  const reserveBtn = document.createElement('button');
+  const movieName = document.createElement('h3');
+  movieName.textContent = name;
+  const img = document.createElement('img');
+  commentBtn.setAttribute('class', 'comment-btn');
+  reserveBtn.setAttribute('class', 'reserve-btn');
+  commentBtn.textContent = 'Comments';
+  reserveBtn.textContent = 'Reservations';
+  btnContainer.setAttribute('class', 'movie-actions');
+  img.setAttribute('src', image.medium);
+  btnContainer.appendChild(reserveBtn);
+  btnContainer.appendChild(commentBtn);
+  return [img, movieName, btnContainer];
+};
+
+// export default BASE_URL;
