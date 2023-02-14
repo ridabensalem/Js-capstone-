0faf2b459f11f645ae6784f2dccd68e199661ea8
Ayo Moses <ayomoses@afrimash.com>
2023-02-14 20:08:45 +0100

    added reservation and comments button to the list of movies

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,16 +2,33 @@ import events from './selectors.js';
 import apiHandler from './fetch.js';
 import BASE_URL from './utils.js';
 
+
+
+
 const displayMovies = (movies) => {
   movies.forEach((movie) => {
-    const { image } = movie.show;
-    const list = document.createElement('li');
-    const img = document.createElement('img');
-    img.setAttribute('src', image.medium);
-    list.setAttribute('class', 'movie-list');
-    list.appendChild(img);
-    events.appendChild(list);
-  });
+  const { image } = movie.show;
+  const list = document.createElement('li');
+  const movieName = document.createElement('h3');
+  movieName.textContent = movie.show.name
+  const img = document.createElement('img');
+  const btnContainer = document.createElement('div')
+  const commentBtn = document.createElement('button')
+  const reserveBtn = document.createElement('button')
+  commentBtn.setAttribute('class', 'comment-btn')
+  reserveBtn.setAttribute('class', 'reserve-btn')
+  btnContainer.setAttribute('class', 'movie-actions')
+  commentBtn.textContent = "Comments"
+  reserveBtn.textContent = "Reservations"
+  img.setAttribute('src', image.medium);
+  list.setAttribute('class', 'movie-list');
+  btnContainer.appendChild(reserveBtn)
+  btnContainer.appendChild(commentBtn)
+  list.appendChild(img);
+  list.appendChild(movieName);
+  list.appendChild(btnContainer)
+  events.appendChild(list);
+});
 };
 
 const getAllEvents = async () => {
