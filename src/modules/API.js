fd28cf0cec0b4a24ba96f43a3ab70faf3511fb70
ridabensalem <ridasbnesalmas@gmail.com>
2023-02-15 22:52:06 +0100

    Fetch the involvement API for getting likes

--- src/modules/API.js
+++ src/modules/API.js
@@ -12,6 +12,17 @@ export const getAllComments = async (id) => {
   return data;
 };
 
+export const getAllLikes = async () => {
+  const response = await fetch(`${INV_URL}/likes`, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const data = await response.json();
+  return data;
+};
+
 const getData = async () => {
   const response = await fetch('GET', BASE_URL);
   const data = await response.json();
