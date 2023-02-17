1d5f6c08ff79474a5f21b4767c9bb3c2041bbe5b
ridabensalem <ridasbnesalmas@gmail.com>
2023-02-17 10:53:22 +0100

    Added more test to the comment counter

--- src/__tests__/commentCounter.test.js
+++ src/__tests__/commentCounter.test.js
@@ -15,3 +15,25 @@ describe('commentsCounter', () => {
     expect(commentsCounter()).toBe(3);
   });
 });
+test('returns 0 if no comments are present', () => {
+  document.body.innerHTML = ' <div>      </div>';
+  expect(commentsCounter()).toBe(0);
+});
+
+test('returns 1 if only one comment is present', () => {
+  document.body.innerHTML = `
+    <div>
+      <div class="comment"></div>
+    </div>
+  `;
+  expect(commentsCounter()).toBe(1);
+});
+
+test('returns 0 if no comments with the given class name are present', () => {
+  document.body.innerHTML = `
+    <div>
+      <div class="other-class"></div>
+    </div>
+  `;
+  expect(commentsCounter()).toBe(0);
+});
